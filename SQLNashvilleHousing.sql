ac3bcbf7c5dbea24c08e4cd5fff56c27912dc33f
Jefka Dhammananda <104891582+jefkadhammananda@users.noreply.github.com>
2022-05-20 04:28:21 +0700

    Update SQLNashvilleHousing.sql

--- SQLNashvilleHousing.sql
+++ SQLNashvilleHousing.sql
@@ -153,6 +153,7 @@ SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
 	   ELSE SoldAsVacant
 	   END
 
+--menggunakan CTE, fungsi di dalam WITH untuk melihat data yang duplikat, lalu selanjutnya akan dilakukan penghapusan terhadap data duplikat tersebut
 --menghilangkan duplikat
 --menggunakan query ROW_NUMBER untuk melihat seberapa banyak data yang sama, bernilai 1 apabila data unique, 2 apabila terdapat 1 duplikat, 3 apabila terdapat 2 duplikat dan seterusnya
 --menggunakan partition untuk memulai perhitungan data pada setiap partisi dan apabila ada data yang duplikat maka rownumber akan bernilai 2 dan untuk melakukan perhitungan rownumber dengan data yang berbeda lagi akan dimulai dari 1 
